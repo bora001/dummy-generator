2b819b5c8f1ceac096409ee66562b3a98fe18064
bora <borado.lee7@gmail.com>
2024-07-19 21:49:42 +0900

    add esbuild

---
+++ build.js
@@ -0,0 +1,26 @@
+import esbuild from "esbuild";
+
+const baseConfig = {
+  entryPoints: ["src/index.ts"],
+  outdir: "dist",
+  bundle: true,
+  sourcemap: true,
+};
+Promise.all([
+  // for cjs
+  esbuild.build({
+    ...baseConfig,
+    format: "cjs",
+    outExtension: {
+      ".js": ".cjs",
+    },
+  }),
+  // for esm
+  esbuild.build({
+    ...baseConfig,
+    format: "esm",
+  }),
+]).catch(() => {
+  console.log("Build failed");
+  process.exit(1);
+});
